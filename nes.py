2db912ce410820c4d3794fd139c1fc6b94a329ae
feelingproudindianarmy <celestialh4@gmail.com>
2024-11-12 17:59:05 +0545

    nested conditions

---
+++ nes.py
@@ -0,0 +1,9 @@
+n=int(input("enter your marks"))
+y=int(input("enter your age"))
+if n>=80:
+    if y>=8:
+        print("You can get 10 persent discount for going to the trip")
+    else:
+        print("You can get 5 persent discount for going to the trip")
+else:
+    print("You cannot get any discount for going to the trip")
---
+++ nested.py
@@ -0,0 +1,8 @@
+n=int(input("enter any number"))
+if n>=0:
+    if n==0:
+        print("Your number is zero")
+    else:
+        print("Your number is positive")
+else:
+    print("Your number is negitive")
